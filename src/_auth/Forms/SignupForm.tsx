abbb36c348222f4023509a73bad4612ff363dae6
najm-choueiry <nachoueiry@outlook.com>
2023-12-21 09:12:49 +0200

    <> Creating the Signup Form component using zod

--- src/_auth/Forms/SignupForm.tsx
+++ src/_auth/Forms/SignupForm.tsx
@@ -1,5 +1,5 @@
-import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
 
 import {useForm} from 'react-hook-form';
 
@@ -8,23 +8,20 @@ import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessag
 } from "@/components/ui/form"
 
 import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+import { SignupValidation } from "@/lib/validation";
 
 const SignupForm = () => {
   
    // 1. Define your form.
-   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+   const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
       username: "",
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
