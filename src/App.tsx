a869d3103e5d1be2227f47da001a3d95c9da7b8b
najm-choueiry <nachoueiry@outlook.com>
2023-12-21 04:09:26 +0200

    <> File structuring and organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import AuthLayout from './_auth/AuthLayout'
 import SigninForm from './_auth/Forms/SigninForm'
 import SignupForm from './_auth/Forms/SignupForm'
 import { Home } from './_root/pages'
@@ -9,8 +10,10 @@ const App = () => {
     <main className='flex h-screen'>
         <Routes>
             {/* public routes */}
-            <Route path="/signin" element={<SigninForm/>}/>
-            <Route path="/signin" element={<SignupForm/>}/>
+            <Route element={<AuthLayout />}>
+                <Route path="/signin" element={<SigninForm/>}/>
+                <Route path="/signin" element={<SignupForm/>}/>
+            </Route>
 
             {/* private routes */}
             <Route index element={<Home/>}/>
