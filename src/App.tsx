a2a46c4b6099896e98fcd5b871d1bf1dbc117e8a
najm-choueiry <nachoueiry@outlook.com>
2023-12-21 04:02:21 +0200

    <> File structuring and organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import SigninForm from './_auth/Forms/SigninForm'
+import SignupForm from './_auth/Forms/SignupForm'
+import { Home } from './_root/pages'
 import './globals.css'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-        Social HUb
-    </h1>
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            <Route path="/signin" element={<SigninForm/>}/>
+            <Route path="/signin" element={<SignupForm/>}/>
+
+            {/* private routes */}
+            <Route index element={<Home/>}/>
+        </Routes>
+    </main>
   )
 }
 
