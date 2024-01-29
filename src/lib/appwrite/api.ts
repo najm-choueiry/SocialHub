b134c32b15c2117995d8ac52896c0f40bb15f22d
najm-choueiry <nachoueiry@outlook.com>
2024-01-29 15:52:34 +0200

    <> Creating the newuseraccount function

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,18 @@
-export async function createUserAccount(user: INew){
+import { INewUser } from "@/types";
+import { account } from "./config";
+import { ID } from "appwrite";
 
+export async function createUserAccount(user: INewUser){
+    try {
+        const newAccount = await account.create(
+           ID.unique(),
+            user.email,
+            user.password,
+            user.name
+        )
+        return newAccount
+    }catch(error){
+        console.log(error)
+        return error
+    }
 }
