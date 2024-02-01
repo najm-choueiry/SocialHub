024eb98b77e7f9f3858090e510b19ec9910bb273
najm-choueiry <nachoueiry@outlook.com>
2024-02-01 13:40:12 +0200

    <> Setting up the database on Appwrite Cloud

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -2,21 +2,23 @@ import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
 
 export const appwriteConfig = {
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    url: import.meta.env.VITE_APPWRITE_URL
+    url: import.meta.env.VITE_APPWRITE_URL,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    userCollectionId:import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID , 
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID , 
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 }
+
 
 const client = new Client();
 
 client.setProject(appwriteConfig.projectId);
 client.setEndpoint(appwriteConfig.url);
 
-console.log("appwriteConfig.projectId", appwriteConfig.projectId)
-console.log("appwriteConfig.url", appwriteConfig.url)
-2
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
 export const avatars = new Avatars(client);
 
-account.listSessions()
