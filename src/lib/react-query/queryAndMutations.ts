98acbc80ae25ecefac087435061f4d54e9c88aaa
najm-choueiry <nachoueiry@outlook.com>
2024-02-01 15:51:14 +0200

    <> Creating a new tanstack query

--- src/lib/react-query/queryAndMutations.ts
+++ src/lib/react-query/queryAndMutations.ts
@@ -2,9 +2,20 @@ import { INewUser } from '@/types'
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
 import { createUserAccount, signInAccount } from '../appwrite/api'
 
+// Creating the account
 export const useCreateUserAccount = () => {
     return useMutation({
         mutationFn:(user: INewUser) => createUserAccount(user)
     })
 }
 
+
+// Sigin into the account
+export const useSignInAccount = () => {
+    return useMutation({
+        mutationFn:(user: {
+            email: string, 
+            password: string
+        }) => signInAccount(user)
+    })
+}
