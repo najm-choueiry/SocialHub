dbd8c9108d58fdf7772cffefa8e3382428c324c8
najm-choueiry <nachoueiry@outlook.com>
2024-04-28 03:05:33 +0300

    <> Defining the form

--- src/components/forms/PostFrom.tsx
+++ src/components/forms/PostFrom.tsx
@@ -2,6 +2,14 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import {useForm} from  'react-hook-form'
 import * as z from 'zod'
 
+import { Button} from '@/components/ui/button'
+
+import {
+    Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage
+} from '@/components/ui/form'
+
+import { Input } from "@/components/ui/input"
+
 const formSchema = z.object({
     username: z.string().min(2, {
         message: "Username must be at leart 2 characters",
@@ -9,6 +17,18 @@ const formSchema = z.object({
 })
 
 const PostFrom = () => {
+
+   const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: ""
+        }
+   }) 
+
+   function onSubmit(values: z.infer<typeof formSchema>) {
+
+   }
+
   return (
     <div>PostFrom</div>
   )
