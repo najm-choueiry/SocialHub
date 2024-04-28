63927a1f6c6f2664982de3c85ea8a7b63b770a73
najm-choueiry <nachoueiry@outlook.com>
2024-04-28 14:38:19 +0300

    <> Working on the file uploader using react dropzone

--- src/components/Shared/FileUploader.tsx
+++ src/components/Shared/FileUploader.tsx
@@ -1,7 +1,11 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
+import { Button } from '../ui/button'
 
 const FileUploader = () => {
+
+
+const  [fileUrl, setFileUrl] = useState('')
 
 const onDrop = useCallback(acceptedFiles => {
     }, [])
@@ -9,12 +13,27 @@ const onDrop = useCallback(acceptedFiles => {
 const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
-    <div {...getRootProps()}>
-        <input {...getInputProps()} />
+    <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
+        <input {...getInputProps()} className='cursor-pointer'/>
         {
-        isDragActive ?
-            <p>Drop the files here ...</p> :
-            <p>Drag 'n' drop some files here, or click to select files</p>
+        fileUrl ? (
+            <div>
+                    test1
+            </div>
+            ) : (
+            <div className='file_uploader-box'>
+                <img 
+                    src="/assets/icons/file-upload.svg"
+                    width={96}
+                    height={77}
+                    alt="file-upload"
+                />
+                <h3 className='base-medium text-light-2 mb-2 mt-6'>Drag photo here</h3>
+                <p className='text-light-4 small-regular mb-6'> SVG, PNG, JPG</p>
+
+                <Button className='shad-button_dark_4'> Select from files</Button>
+            </div>
+            )
         }
   </div>
   )
