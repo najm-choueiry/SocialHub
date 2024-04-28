20f000cab9cd40fc6c16a504451997579fe62951
najm-choueiry <nachoueiry@outlook.com>
2024-04-28 15:17:36 +0300

    <> Displaying the image when inserted

--- src/components/Shared/FileUploader.tsx
+++ src/components/Shared/FileUploader.tsx
@@ -10,30 +10,37 @@ type FileUploaderProps = {
 const FileUploader = ({fieldChange, mediaUrl}: FileUploaderProps) => {
 
 
-const  [fileUrl, setFileUrl] = useState('')
-const  [file, setFile] = useState([]);
+    const  [fileUrl, setFileUrl] = useState('')
+    const  [file, setFile] = useState<File[]>([]);
 
-const onDrop = useCallback((acceptedFiles : FileWithPath) => {
-    setFile(acceptedFiles)
-    fieldChange(acceptedFiles)
-    setFileUrl(URL.createObjectURL(acceptedFiles[0]))
-    }, [file])
+    const onDrop = useCallback((acceptedFiles : FileWithPath[]) => {
+        setFile(acceptedFiles)
+        fieldChange(acceptedFiles)
+        setFileUrl(URL.createObjectURL(acceptedFiles[0]))
+        }, [file])
 
-const {getRootProps, getInputProps} = useDropzone({
-        onDrop , 
-        accept: {
-        'image/*': ['.png' , '.jpeg', '.jpg', '.svg']
-        }
-    })
+    const {getRootProps, getInputProps} = useDropzone({
+            onDrop , 
+            accept: {
+            'image/*': ['.png' , '.jpeg', '.jpg', '.svg']
+            }
+        })
 
   return (
     <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
         <input {...getInputProps()} className='cursor-pointer'/>
         {
         fileUrl ? (
-            <div>
-                    test1
+            <>
+            <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+                <img 
+                    src={fileUrl}
+                    alt="image"
+                    className='file_uploader-img'
+                />
             </div>
+            <p className='file_uploader-label'> Click or drag photo to replace </p>
+            </>
             ) : (
             <div className='file_uploader-box'>
                 <img 
