46cef42c6a69daa91256b0af02bad3d73fd27a32
najm-choueiry <nachoueiry@outlook.com>
2024-04-28 14:30:35 +0300

    <> Working on the file uploader using react dropzone

--- src/components/Shared/FileUploader.tsx
+++ src/components/Shared/FileUploader.tsx
@@ -1,6 +1,22 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
 const FileUploader = () => {
+
+const onDrop = useCallback(acceptedFiles => {
+    }, [])
+
+const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+        isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+  </div>
   )
 }
 
