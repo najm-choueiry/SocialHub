fd1dba7cc24a3441f1fd874588352ed2acaa9a7b
najm-choueiry <nachoueiry@outlook.com>
2024-04-28 03:13:12 +0300

    <> Defining the form

--- src/components/forms/PostFrom.tsx
+++ src/components/forms/PostFrom.tsx
@@ -9,6 +9,7 @@ import {
 } from '@/components/ui/form'
 
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 
 const formSchema = z.object({
     username: z.string().min(2, {
@@ -31,23 +32,36 @@ const PostFrom = () => {
 
    return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} 
+        className="flex-col gap-9 w-full max-w-5xl">
         <FormField
           control={form.control}
-          name="username"
+          name="caption"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel className="shad-form_label">Caption</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Textarea placeholder="shadcn" className="shad-textarea custom-scrollbar" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
+              <FormMessage className="shad-form_message"/>
             </FormItem>
           )}
         />
+
+    <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Photos</FormLabel>
+              <FormControl>
+                <FileUploader />
+              </FormControl>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
