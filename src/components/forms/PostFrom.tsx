8bcbcb6a29c58aaeb7d5dcc42128e5810a719169
najm-choueiry <nachoueiry@outlook.com>
2024-04-28 14:25:38 +0300

    <> Modifying the post form

--- src/components/forms/PostFrom.tsx
+++ src/components/forms/PostFrom.tsx
@@ -33,71 +33,74 @@ const PostFrom = () => {
    }
 
    return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} 
-        className="flex-col gap-9 w-full max-w-5xl">
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} 
+            className="flex flex-col gap-9 w-full max-w-5xl">
+            <FormField
+            control={form.control}
+            name="caption"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel className="shad-form_label">Caption</FormLabel>
+                <FormControl>
+                    <Textarea placeholder="shadcn" className="shad-textarea custom-scrollbar" {...field} />
+                </FormControl>
+                <FormMessage className="shad-form_message"/>
+                </FormItem>
+            )}
+            />
+
         <FormField
-          control={form.control}
-          name="caption"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="shad-form_label">Caption</FormLabel>
-              <FormControl>
-                <Textarea placeholder="shadcn" className="shad-textarea custom-scrollbar" {...field} />
-              </FormControl>
-              <FormMessage className="shad-form_message"/>
-            </FormItem>
-          )}
-        />
+            control={form.control}
+            name="file"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel className="shad-form_label">Add Photos</FormLabel>
+                <FormControl>
+                    <FileUploader />
+                </FormControl>
+                <FormMessage className="shad-form_message"/>
+                </FormItem>
+            )}
+            />
 
-    <FormField
-          control={form.control}
-          name="file"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="shad-form_label">Add Photos</FormLabel>
-              <FormControl>
-                <FileUploader />
-              </FormControl>
-              <FormMessage className="shad-form_message"/>
-            </FormItem>
-          )}
-        />
+        <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel className="shad-form_label">Add Location</FormLabel>
+                <FormControl>
+                    <Input type="text" className="shad-input" />
+                </FormControl>
+                <FormMessage className="shad-form_message"/>
+                </FormItem>
+            )}
+            />
 
-    <FormField
-          control={form.control}
-          name="location"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="shad-form_label">Caption</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" />
-              </FormControl>
-              <FormMessage className="shad-form_message"/>
-            </FormItem>
-          )}
-        />
+        <FormField
+            control={form.control}
+            name="tags"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel className="shad-form_label">Add tags (seperated by comma " , ")</FormLabel>
+                <FormControl>
+                    <Input 
+                        type="text"
+                        className="shad-input"
+                        placeholder="Art, Expression, Learn"
+                    />
+                </FormControl>
+                <FormMessage className="shad-form_message"/>
+                </FormItem>
+            )}
+            />
 
-    <FormField
-          control={form.control}
-          name="tags"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="shad-form_label">Add tags (seperated by comma " , ")</FormLabel>
-              <FormControl>
-                <Input 
-                    type="text"
-                    className="shad-input"
-                    placeholder="Art, Expression, Learn"
-                />
-              </FormControl>
-              <FormMessage className="shad-form_message"/>
-            </FormItem>
-          )}
-        />
+        <div className="flex gap-4 items-center justify-end">
+            <Button type="button" className="shad-button_dark_4">Cancel</Button>
+            <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+        </div>
 
-
-        <Button type="submit">Submit</Button>
       </form>
     </Form>
   )
